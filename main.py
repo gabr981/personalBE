0a06df922d0ac77a51441b92947977d90967f4f2
gabr981 <gabriele@finhubapp.com>
2024-10-21 20:40:54 +0200

    non so

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from pydantic import BaseModel
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Carica variabili d'ambiente da un file .env
-MONGO_URL=os.getenv("MONGO_URL", "mongodb://localhost:27017")
+#load_dotenv()  # Carica variabili d'ambiente da un file .env
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 API_KEY = os.getenv("API_KEY")
 
-app = FastAPI()
+app = FastAPI(root_path="/api")  # Aggiungi root_path
 
-api = APIRouter(prefix="/api")
+api = APIRouter()
 
 
 
@@ -26,6 +26,10 @@ class Item(BaseModel):
 
 # Middleware per l'API Key
 
+@api.get("/test")
+async def test_route():
+    return "Route di test funzionante"
+
 async def api_key_auth(api_key: str = Header(None)):
     if api_key != API_KEY:
         raise HTTPException(status_code=403, detail="API Key non valida")
@@ -34,12 +38,8 @@ async def api_key_auth(api_key: str = Header(None)):
 @api.get("/items", dependencies=[Depends(api_key_auth)])
 async def read_items():
     #items = await db.items.find().to_list(100)
-    return "items"
+    return "Ciao questo è un test e se mi vedi vuol dire che funziona"
 
-@api.post("/item", dependencies=[Depends(api_key_auth)])
-async def create_item(item: Item):
-    result = await db.items.insert_one(item.dict())
-    return {"id": str(result.inserted_id)}
 
 
 app.include_router(api)
